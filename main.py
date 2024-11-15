f27bb8e14656d116aeecf87d2e2bee8c2482de7b
space dr house <92742289+spacedrhouse@users.noreply.github.com>
2024-11-15 08:54:25 +0100

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import toolbox as tb
-import warnings
-
-if __name__ == "__main__":
-    warnings.filterwarnings('ignore')
-    [coords, x_coord, y_coord, x_node, y_node, Q, W, VOL, LC, cast, internal, Z, Z_, wp_list, MP] = tb.input_data()
-    [Class, P, G, C, Stock_Zones, Cast_Zones, num_columns_per_zone, column_data, dis, L, A, apc, VOL, LC, cast, internal, MP] = tb.data_manipulation(coords, x_coord, y_coord, x_node, y_node, Q, W, VOL, LC, cast, internal, Z, Z_, wp_list, MP)
-    [mdl, sol, x, y, L, P] = tb.solve(Class, P, G, C, Stock_Zones, Cast_Zones, num_columns_per_zone, column_data, dis, L, A, apc, VOL, LC, cast, internal, MP)
-    tb.plotting(mdl, sol, x, y, L, P, MP, Stock_Zones, x_coord, y_coord, num_columns_per_zone, column_data, wp_list, Z_)
